776d84495376956ffc5ca830bf92fb29ef50c9b3
Olga Mikhailova <foxx_fire@mail.ru>
2025-07-11 00:38:03 +0300

    settings

---
+++ iOS-HW18-Olga Mikhailova/PomodoroViewController/PomodoroViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class PomodoroViewController: UIViewController {
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+   
+  }
+}
---
+++ iOS-HW18-Olga Mikhailova/Sources/SceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  SceneDelegate.swift
+//  iOS-HW18-Olga Mikhailova
+//
+//  Created by FoxxFire on 10.07.2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+
+
+  func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
+    
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    
+    let window = UIWindow(windowScene: windowScene)
+    let viewController = PomodoroViewController()
+    window.rootViewController = viewController
+    self.window = window
+    window.makeKeyAndVisible()    
+  }
+}
+
