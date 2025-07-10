07aa4461d53ac665bf3c30823036ab6b07d8f7b9
Olga Mikhailova <foxx_fire@mail.ru>
2025-07-10 21:17:02 +0300

    Initial Commit

---
+++ iOS-HW18-Olga Mikhailova/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-HW18-Olga Mikhailova
+//
+//  Created by FoxxFire on 10.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
